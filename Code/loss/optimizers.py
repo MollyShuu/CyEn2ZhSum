5f5dd27e93b8a863c6c04f61c44cf612eeda8c3a
MollyShuu <41218685+MollyShuu@users.noreply.github.com>
2022-11-19 15:39:47 +0800

    Add files via upload

--- Code/loss/optimizers.py
+++ Code/loss/optimizers.py
@@ -20,10 +20,8 @@ class WarmAdam(object):
         self.original_lr = lr
         self.n_step = n_step
         self.hidden_size = hidden_size
-        self.warm_up_step = warm_up  # 超过该值后学习率开始下降
+        self.warm_up_step = warm_up  
         self.optimizer = optim.Adam(params, betas=[0.9, 0.998], eps=1e-8)
-
-    #  self.scheduler = optim.lr_scheduler.CosineAnnealingLR(self.optimizer, T_max=500, eta_min=1e-8, last_epoch=-1)
 
     def step(self):
         self.n_step = self.n_step + 1
@@ -33,29 +31,23 @@ class WarmAdam(object):
             param_group['lr'] = lr
         self.optimizer.step()
 
-    # # -----采用余弦退火学习率----#
-    # def step(self):
-    #     self.n_step = self.n_step + 1
-    #     self.optimizer.step()
-    #     self.scheduler.step()
 
 
 class LabelSmoothingLoss(nn.Module):
     def __init__(self, label_smoothing, tgt_vocab_size, ignore_index):
         self.padding_idx = ignore_index
-        self.label_smoothing = label_smoothing  # 0.1
+        self.label_smoothing = label_smoothing  
         self.vocab_size = tgt_vocab_size
 
         super(LabelSmoothingLoss, self).__init__()
 
-    # 如果要改，生成改成词向量，dec_outputs也应该改成词向量
+
     def forward(self, output, target):
-        target = target[:, 1:].contiguous().view(-1)  # 展开铺平成1维，取个1：是因为在NLCS中tgt开头为<bos>
-        output = output.view(-1, self.vocab_size)  # size [248]
-        # output = output[:, 0:-1].contiguous().view(-1, self.vocab_size)  # size [248, 5003]
-        non_pad_mask = target.ne(self.padding_idx)  # .ne函数，逐一比较是否‘不相同’
+        target = target[:, 1:].contiguous().view(-1)  
+        output = output.view(-1, self.vocab_size)  
+        non_pad_mask = target.ne(self.padding_idx)  
         nll_loss = -output.gather(dim=-1, index=target.view(-1, 1))[
-            non_pad_mask].sum()  # [248,1],按index在dim维度上取值，输出和index维度相同
+            non_pad_mask].sum()
         smooth_loss = -output.sum(dim=-1, keepdim=True)[non_pad_mask].sum()
         eps_i = self.label_smoothing / self.vocab_size
         loss = (1. - self.label_smoothing) * nll_loss + eps_i * smooth_loss
@@ -65,14 +57,14 @@ class MSELoss(nn.Module):
     def __init__(self, ignore_vec):
         super(MSELoss, self).__init__()
         self.padding_vec = ignore_vec
-        self.linear = nn.Linear(1, 1, bias=False)  # loss中的λ参数，自我学习
+        self.linear = nn.Linear(1, 1, bias=False)  
 
     def forward(self, output, target):
-        target = target.view(-1, target.size(-1))  # [batch_size*tgt_len,embedding_dim]
-        output = output.view(-1, target.size(-1))  # [batch_size*tgt_len,embedding_dim]
-        non_pad_mask = target.ne(self.padding_vec)  # torch.tensor(pad_vecs["tgt"]),判断是否不相同
-        non_pad_mask = [True in vec for vec in non_pad_mask]  # [batch_size*tgt_len], 只要有1个维度不相同则不为pad
-        loss = torch.mean((output - target).abs()[non_pad_mask].sum(dim=1) ** 2)  # MSELoss = (x-y)^2
+        target = target.view(-1, target.size(-1))
+        output = output.view(-1, target.size(-1))  
+        non_pad_mask = target.ne(self.padding_vec)
+        non_pad_mask = [True in vec for vec in non_pad_mask]  
+        loss = torch.mean((output - target).abs()[non_pad_mask].sum(dim=1) ** 2)
         # 从语义上，应该加上整句相似的惩罚
         return loss
 
