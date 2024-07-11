3fcdf06270cba83cff439864d8fb2429f14a9519
MollyShuu <41218685+MollyShuu@users.noreply.github.com>
2024-07-11 18:20:18 +0800

    Update metric.py

--- Code/Utils/metric.py
+++ Code/Utils/metric.py
@@ -17,8 +17,6 @@ def calculate_bleu(hypotheses, references, lowercase=False):
 def file_bleu(hypothesis, reference, lowercase=False):
     # ../../../tools/multi-bleu.perl, so take 3 levels up.
     beaver_path = os.path.dirname(os.path.dirname(__file__))
-     # os.path.dirname(os.path.dirname(os.path.dirname(__file__))) # /home/jsy
-    # print(os.path.dirname(os.path.dirname(__file__))) #/home/jsy/GraphSum
     multi_bleu_path = os.path.join(beaver_path, "tools", "multi-bleu.perl")
     with open(hypothesis, "r") as read_pred, open(os.devnull, "w") as black_hole:
         bleu_cmd = ["perl", multi_bleu_path]
